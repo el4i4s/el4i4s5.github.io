8e7bab148104c9c67fb88618faeced9fa43e7508
el4i4s <elias.seitz@gmail.com>
2021-08-12 21:20:14 -0400

    entrega final
    Elias Seitz Valdes

---
+++ commlab/entrega final/js/script.js
@@ -0,0 +1,12 @@
+function demanda1() {
+    document.getElementById('demanda').src="imagenes/demanda1.jpeg";
+}
+function demanda2() {
+    document.getElementById('demanda').src="imagenes/demanda2.jpeg";
+}
+function vista1() {
+    document.getElementById('vista').src="imagenes/vista1.jpeg";
+}
+function vista2() {
+    document.getElementById('vista').src="imagenes/vista2.jpeg";
+}
